3633206171725564fc8aad227d62a1cc11847904
unknown <kc654723@gmail.com>
2023-01-04 16:00:51 -0500

    more shop items

--- events/item.js
+++ events/item.js
@@ -1,4 +1,7 @@
 module.exports = [{
+    item: 'Water',
+    price: 1
+},{
     item: 'Banana',
     price: 10000
 },{
@@ -7,4 +10,10 @@ module.exports = [{
 },{
     item: 'Pear',
     price: 10000000
+},{
+    item: 'Doge',
+    price: 1000000000
+},{
+    item: 'Moge',
+    price: 1000000000000
 }]
